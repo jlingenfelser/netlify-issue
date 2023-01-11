5184d1e1d4bd31f3837001f3abc87d10aca3cfca
jlingenfelser <johnlingenfelser@gmail.com>
2023-01-11 14:20:11 -0500

    remove sensitive info

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('https://saltmarsh.app');
+export const pb = new PocketBase('someurl');
 
 export const currentUser = writable(pb.authStore.model);
